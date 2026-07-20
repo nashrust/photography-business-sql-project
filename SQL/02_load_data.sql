55e66e670572292ff3b1fd468cc686a4da540e05
nashrust <nashrust18@yahoo.com>
2026-07-20 13:42:00 -0500

    Update and rename 02_insert_raw_data.sql to 02_load_data.sql

---
+++ SQL/02_load_data.sql
@@ -0,0 +1,22 @@
+/*
+====================================================
+02_load_data.sql
+====================================================
+
+Project Data Loading Process
+
+The raw datasets for this project were manually created to
+simulate a photography business database.
+
+The CSV files included in the Data folder were imported into
+PostgreSQL before the data cleaning process began.
+
+Files:
+- clients_raw_data.csv
+- sessions_raw_data.csv
+- package_raw_data.csv
+- booking_raw_data.csv
+
+The imported data was then cleaned, validated, and transformed
+into analytics-ready tables.
+*/
